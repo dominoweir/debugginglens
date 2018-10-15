de8b2ea28b6b5763e164fae0e70d44b5aaa4146f
Domino Rose Weir <dominoweir@gmail.com>
2018-10-15 13:25:55 -0400

    initial commit

---
+++ src/src/DebuggingLens.java
@@ -0,0 +1,15 @@
+package src;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DebuggingLens extends JComponent {
+    public DebuggingLens(){
+
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+    }
+}
