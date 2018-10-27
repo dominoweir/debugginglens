2970e22b1b7b5141741eda2c6538deb645cdb8de
Benjamin du Preez <benjamindupreez@gmail.com>
2018-10-27 16:06:06 -0400

    created initial control panel frame

--- src/lens/DebuggingLens.java
+++ src/lens/DebuggingLens.java
@@ -1,23 +1,80 @@
 package lens;
 
+import javafx.scene.control.CheckBox;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.MouseEvent;
+import javax.swing.border.TitledBorder;
 import javax.swing.event.MouseInputAdapter;
 
 class DebuggingLens extends JComponent implements ItemListener {
 
-    Point point;
+    // used to save cursor position
+    private Point point;
+    private int width, height; // width and height of the debugging lens
+
+    // Control Panel stuff
+    private JFrame lensControlPanel;
+    private JCheckBox borderLocationsFilt, borderWidthsFilt, componentSizesFilt, componentClassesFilt, fontMetricsFilt, layoutManagerFilt;
+    private JPanel filtersPnl;
+    private TitledBorder filtersBorder;
 
     public DebuggingLens(AbstractButton aButton, Container contentPane) {
         CheckBoxListener listener = new CheckBoxListener(aButton,this, contentPane);
         addMouseListener(listener);
         addMouseMotionListener(listener);
+
+        // init lens size
+        width = 100;
+        height = 100;
+
+        // control panel stuff
+        initCtrlPnl();
+        setupCtrlPnl();
     }
 
-    // react to change button clicks.
+    // construct the components on the control panel
+    private void initCtrlPnl() {
+
+        // control panel
+        lensControlPanel = new JFrame("Debugging Lens Control Panel");
+        filtersPnl = new JPanel();
+            filtersBorder = new TitledBorder("Filters");
+            filtersBorder.setTitleJustification(TitledBorder.LEFT);
+            filtersBorder.setTitlePosition(TitledBorder.TOP);
+            filtersPnl.setBorder(filtersBorder);
+
+            borderLocationsFilt = new JCheckBox("Border Locations");
+            borderWidthsFilt = new JCheckBox("Border Widths");
+            componentSizesFilt = new JCheckBox("Component Sizes");
+            componentClassesFilt = new JCheckBox("Component Classes");
+            fontMetricsFilt = new JCheckBox("Component Sizes");
+            layoutManagerFilt = new JCheckBox("Component Classes");
+
+        lensControlPanel.pack();
+        lensControlPanel.setVisible(true);
+
+    }
+    // setup and customize the components on the control panel
+    private void setupCtrlPnl() {
+        lensControlPanel.setLayout(new GridLayout(1, 1));
+        lensControlPanel.add(filtersPnl);
+            filtersPnl.setLayout(new GridLayout(6, 1));
+            filtersPnl.add(borderLocationsFilt);
+            filtersPnl.add(borderWidthsFilt);
+            filtersPnl.add(componentSizesFilt);
+            filtersPnl.add(componentClassesFilt);
+            filtersPnl.add(fontMetricsFilt);
+            filtersPnl.add(layoutManagerFilt);
+
+        lensControlPanel.pack();
+        lensControlPanel.setVisible(true);
+    }
+
+    // react to change button clicks
     public void itemStateChanged(ItemEvent e) {
         setVisible(e.getStateChange() == ItemEvent.SELECTED);
     }
@@ -25,7 +82,7 @@ class DebuggingLens extends JComponent implements ItemListener {
     protected void paintComponent(Graphics g) {
         if (point != null) {
             g.setColor(Color.black);
-            g.drawRect(point.x, point.y, 100, 100);
+            g.drawRect(point.x, point.y, width, height);
         }
     }
 
